8fd4d9e3e74a5f4e3706dc7004bc041a49c9b51a
neeteshmarkam7162@gmail.com <neeteshmarkam7162@gmail.com>
2026-03-15 22:16:37 +0530

    moodify

---
+++ moodify/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from './feactures/Expression/components/FaceExpression'
+
+const App = () => {
+  return (
+
+      <FaceExpression></FaceExpression>
+   
+  )
+}
+
+export default App
